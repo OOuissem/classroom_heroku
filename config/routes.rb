c901742a76544aa03c8247135f468525a05cf608
OOuissem <smartmen0909@gmail.com>
2017-01-31 12:53:47 +0100

    add routes to courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+
+  get 'courses/show/:id', to: 'courses#show', as: 'course'
+  
   get 'students/index'
 
-  get 'students/show/:id', :to => "students#show"
+  get 'students/show'
 
-  root "students#index"
+  root "courses#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
